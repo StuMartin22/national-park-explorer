59b26f54e063a6ff871ce6b591715d0e7206a40f
Eric <ericphilipps123@gmail.com>
2022-05-31 14:25:00 -0500

    backend login

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,12 @@ import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import AuthService from "./services/auth.service";
 import Login from "./components/login.component";
-import Register from "./components/register.component";
-import Home from "./components/home.component";
-import Profile from "./components/profile.component";
-import BoardUser from "./components/board-user.component";
-import BoardModerator from "./components/board-moderator.component";
-import BoardAdmin from "./components/board-admin.component";
+// import Register from "./components/register.component";
+// import Home from "./components/home.component";
+// import Profile from "./components/profile.component";
+// import BoardUser from "./components/board-user.component";
+// import BoardModerator from "./components/board-moderator.component";
+// import BoardAdmin from "./components/board-admin.component";
 class App extends Component {
   constructor(props) {
     super(props);
