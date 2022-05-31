a1016720d3b6ee27f576c42e16c26085f9898842
Eric <ericphilipps123@gmail.com>
2022-05-31 11:26:21 -0500

    Merging

--- server/seeds/schema/resolvers.js
+++ server/seeds/schema/resolvers.js
@@ -1,10 +1,46 @@
 const { Constant } = require('../models');
+const { User } = require('../../models');
 
 const resolvers = {
     Query: {
-        ourThing: async () => {
-           return await Constant.find({});
-        }
+        users: async () => {
+           return await User.find();
+        },
+
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId });
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await user.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user with this email found!');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+        },
+        removeUser: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOneAndDelete({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
     }
 }
 
