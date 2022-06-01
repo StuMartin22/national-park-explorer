35fe30f077628bf9257a4af13f46b369df5b745a
Brady Mavetz <b.mavetz@outlook.com>
2022-06-01 12:58:25 -0500

    removed duplicate schema file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,22 @@
-const { User } = require('../models');
+const { User, Comment } = require('../models');
+const { signToken } = require('../utils/auth');
 
+// 
 const resolvers = {
     Query: {
         users: async () => {
            return await User.find();
         },
-
         user: async (parent, { userId }) => {
             return User.findOne({ _id: userId });
         },
+        comments: async (parent, { parkCode }) => {
+          const params = parkCode ? { parkCode } : {};
+          return await Comment.find(params);
+        },
+        comment: async (parent, { commentID }) => {
+          return Comment.findOne({ _id: commentId });
+        }
     },
 
     Mutation: {
@@ -40,6 +48,31 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
           },
+        addComment: async (parent, { commentText, commentAuthor, parkCode }) => {
+          const comment = await Comment.create({ commentText, commentAuthor, parkCode });
+
+          await User.findOneAndUpdate(
+            { username: commentAuthor },
+            { $addToSet: { comments: comment._id }}
+          );
+        },
+        removeComment: async (parent, { commentId }) => {
+          return await Comment.findOneAndDelete({ _id: commentId });
+        },
+        addRating: async (parent, {commentId, ratingNumber, ratingAuthor}) => {
+          Comment.findOneAndUpdate(
+            { _id: commentId },
+            { $addToSet: { ratings: { ratingNumber, ratingAuthor }}},
+            { new: true }
+          )
+        },
+        removeRating: async (parent, { commentId, ratingId }) => {
+          return await Comment.findOneAndUpdate(
+            { _id: commentId },
+            { $pull: { comments: { _id: ratingId }}},
+            { new: true }
+          );
+        }
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,21 @@ const typeDefs = gql`
         username: String
         email: String
         password: String
+        comments: [Comment]!
+    }
+
+    type Comment {
+        _id: ID
+        commentText: String
+        commentAuthor: String
+        createdAt: String
+        ratings: [Rating]!
+    }
+
+    type Rating {
+        _id: ID
+        ratingNumber: Number
+        ratingAuthor: String
     }
 
     type Auth {
@@ -16,13 +31,18 @@ const typeDefs = gql`
     type Query {
         users: [User]!
         user(userId: ID!): User
+        comments(parkCode: String): [Comment]
+        comment(commentId: ID!): Comment
     }
 
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
-        login(email: String!, password: String!): Auth
-
         removeUser: User
+        login(email: String!, password: String!): Auth
+        addComment(commentText: String!, commentAuthor: String!, parkCode: String!): Comment
+        removeComment(commentId: ID!): Comment
+        addRating(commentId: ID!, ratingNumber: Number!, ratingAuthor: String!): Comment
+        removeRating(commentId: ID!, ratingId: ID!): Comment
   }`;
 
 module.exports = typeDefs;
