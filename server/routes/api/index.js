404446a4289516f77a1262a23fe47f3b7e65d7a5
Brady Mavetz <b.mavetz@outlook.com>
2022-06-01 11:29:58 -0500

    modifying server.js and rename schema file to run backend

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const constant1 = require('./routename1');
-const constant2 = require('./routename2.js.js');
-
-router.use('/route1', constant1);
-router.use('/route2', constant2);
-
-module.exports = router;
