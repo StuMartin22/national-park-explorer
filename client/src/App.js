3f1f4d92a36ba369fe6bbf3f4381169b66ba273e
Elizabeth <ekkjohnson@gmail.com>
2022-06-01 11:24:13 -0500

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,68 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import Login from "./pages/Login";
-import Nav from "./components/Nav";
-import {BrowserRouter, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import Register from "./pages/Register";
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowerRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-    const [name, setName] = useState('');
-
-    useEffect(() => {
-        (
-            async () => {
-                const response = await fetch('http://localhost:8000/api/user', {
-                    headers: {'Content-Type': 'application/json'},
-                    credentials: 'include',
-                });
-
-                const content = await response.json();
-
-                setName(content.name);
-            }
-        )();
-    });
-
-
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Nav name={name} setName={setName}/>
-
-                <main className="form-signin">
-                    <Route path="/" exact component={() => <Home name={name}/>}/>
-                    <Route path="/login" component={() => <Login setName={setName}/>}/>
-                    <Route path="/register" component={Register}/>
-                </main>
-            </BrowserRouter>
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />}
+              />
+              <Route 
+                path="/login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-    );
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
