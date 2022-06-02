be89187cb7511837889888dd2b2ba9223c20a485
Elizabeth <ekkjohnson@gmail.com>
2022-06-02 07:42:12 -0500

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,10 @@ import Login from './pages/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import Search from './components/searchbar';
+
+
+
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
