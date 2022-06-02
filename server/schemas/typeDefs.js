07f57c4e6e0be25c8645dfdabcade8587378029a
Brady Mavetz <b.mavetz@outlook.com>
2022-06-02 11:03:08 -0500

    typedefs merged

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,13 +39,6 @@ const typeDefs = gql`
         addUser(username: String!, email: String!, password: String!): Auth
         removeUser: User
         login(email: String!, password: String!): Auth
-<<<<<<< HEAD
-        addComment(commentText: String!, commentAuthor: String!, parkCode: String!): Comment
-        removeComment(commentId: ID!): Comment
-        addRating(commentId: ID!, ratingNumber: Int!, ratingAuthor: String!): Comment
-        removeRating(commentId: ID!, ratingId: ID!): Comment
-  }`;
-=======
 
         addComment(commentText: String!, commentAuthor: String!, parkCode: String!): Comment
         removeComment(commentId: ID!): Comment
@@ -53,6 +46,5 @@ const typeDefs = gql`
         addRating(commentId: ID!, ratingNumber: Int!, ratingAuthor: String!): Comment
         removeRating(commentId: ID!, ratingId: ID!): Comment
     }`;
->>>>>>> main
 
 module.exports = typeDefs;
