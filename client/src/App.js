671145aa9761e78b47908f0f0e592734ec0e7b38
Stuart (Stu) Martin <stucodes22@gmail.com>
2022-06-05 15:43:06 -0500

    deleted excess code, added a class for styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { MemoryRouter, Switch, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
@@ -8,59 +7,14 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import HomePage from './Pages/Home';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-// import Container from 'react-bootstrap/Container';
-// import Button from 'react-bootstrap/Button';
-// import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-// import { LinkContainer } from 'react-router-bootstrap';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Header from './components/Header';
-// import Footer from './components/Footer';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer';
 import HomePage from './pages/Home'
 import Search from './pages/Search'
 
-    // <MemoryRouter>
-    //   <Container className="p-3">
-    //     <Container className="p-5 mb-4 bg-light rounded-3">
-    //       <h1 className="header">Welcome To React-Bootstrap</h1>
-    //       <h2>
-    //         Current Page is{' '}
-    //         <Switch>
-    //           <Route path="/about">
-    //             <About />
-    //           </Route>
-    //           <Route path="/users">
-    //             <Users />
-    //           </Route>
-    //           <Route path="/">
-    //             <Home />
-    //           </Route>
-    //         </Switch>
-    //       </h2>
-    //       <h2>
-    //         Navigate to{' '}
-    //         <ButtonToolbar className="custom-btn-toolbar">
-    //           <LinkContainer to="/">
-    //             <Button>Home</Button>
-    //           </LinkContainer>
-    //           <LinkContainer to="/about">
-    //             <Button>About</Button>
-    //           </LinkContainer>
-    //           <LinkContainer to="/users">
-    //             <Button>Users</Button>
-    //           </LinkContainer>
-    //         </ButtonToolbar>
-    //       </h2>
-    //     </Container>
-    //   </Container>
-    // </MemoryRouter>
-//   );
-
-export default App;
 
 const httpLink = createHttpLink({
   uri: '/graphql'
@@ -109,12 +63,12 @@ function App() {
               />
             </Routes>
           </div>
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </Router>
     </ApolloProvider> 
   );
 }
-      
-// export default App;
+
+export default App;
 
